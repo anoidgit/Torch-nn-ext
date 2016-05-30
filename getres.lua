740a1d763ef43d0d13c2fa6208028dded8e70792
ano <anoidgit@users.noreply.github.com>
2016-05-30 09:07:04 +0800

    Update getres.lua

--- getres.lua
+++ getres.lua
@@ -5,8 +5,10 @@ function getresmodel(modelcap,scale)
 	rtm:add(modelcap)
 	if not scale then
 		rtm:add(nn.Identity())
-	else
+	elseif type(scale)=='number' then
 		rtm:add(nn.Sequential():add(nn.Identity()):add(nn.MulConstant(scale,true)))
+	else
+		rtm:add(nn.Sequential():add(nn.Identity()):add(scale))
 	end
 	return nn.Sequential():add(rtm):add(nn.CAddTable())
 end
