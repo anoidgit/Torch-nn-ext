8d89763ed42b5a6547f74b77bf1e9549867ff85c
ano <anoidgit@users.noreply.github.com>
2016-06-13 18:21:29 +0800

    Update getmaxout.lua
    This implement is more efficient and simpler.

--- getmaxout.lua
+++ getmaxout.lua
@@ -1,11 +1,6 @@
 require "nn"
 
 function getmaxout(inputs,outputs,nlinear)
-	local rtm=nn.ConcatTable()
 	local ncyc=nlinear or 2
-	for i=1,ncyc do
-		local tmpli=nn.Linear(inputs,outputs)
-		rtm:add(tmpli:clone())
-	end
-	return nn.Sequential():add(rtm):add(nn.JoinTable(2)):add(nn.Reshape(ncyc,outputs,true)):add(nn.Max(2))
+	return nn.Sequential():add(nn.Linear(inputs,outputs*ncyc)):add(nn.Reshape(ncyc,outputs,true)):add(nn.Max(2))
 end
