f106fcc2893690c2f9ad2b9dd55bf015deae64bf
ano <anoidgit@users.noreply.github.com>
2016-10-17 16:24:51 +0800

    Update vecLookup.lua

--- vecLookup.lua
+++ vecLookup.lua
@@ -93,31 +93,33 @@ function vecLookup:updateGradInput(input, gradOutput)
 end
 
 function vecLookup:accGradParameters(input, gradOutput, scale)
-	self:backCompatibility()
-	input = self.copiedInput and self._input or input
-	if input:dim() == 2 then
-		input = input:view(-1)
-	elseif input:dim() ~= 1 then
-		error("input must be a vector or matrix")
-	end
+	if self.updatevec then
+		self:backCompatibility()
+		input = self.copiedInput and self._input or input
+		if input:dim() == 2 then
+			input = input:view(-1)
+		elseif input:dim() ~= 1 then
+			error("input must be a vector or matrix")
+		end
 
-	if not gradOutput:isContiguous() then
-		self._gradOutput = self._gradOutput or gradOutput.new()
-		self._gradOutput:resizeAs(gradOutput):copy(gradOutput)
-		gradOutput = self._gradOutput
-	end
+		if not gradOutput:isContiguous() then
+			self._gradOutput = self._gradOutput or gradOutput.new()
+			self._gradOutput:resizeAs(gradOutput):copy(gradOutput)
+			gradOutput = self._gradOutput
+		end
 
-	self.gradWeight.THNN.LookupTable_accGradParameters(
-		input:cdata(),
-		gradOutput:cdata(),
-		self.gradWeight:cdata(),
-		self._count:cdata(),
-		THNN.optionalTensor(self._sorted),
-		THNN.optionalTensor(self._indices),
-		self.shouldScaleGradByFreq or false,
-		self.paddingValue or 0,
-		scale or 1
-	)
+		self.gradWeight.THNN.LookupTable_accGradParameters(
+			input:cdata(),
+			gradOutput:cdata(),
+			self.gradWeight:cdata(),
+			self._count:cdata(),
+			THNN.optionalTensor(self._sorted),
+			THNN.optionalTensor(self._indices),
+			self.shouldScaleGradByFreq or false,
+			self.paddingValue or 0,
+			scale or 1
+		)
+	end
 end
 
 function vecLookup:renorm(input)
